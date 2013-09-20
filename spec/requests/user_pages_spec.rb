4fc4260ce0ca2279774b988984eae19b4662486e
Michael Aronzon <michael.aronzon@gmail.com>
2013-09-19 22:53:03 -0400

    Add Edit form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,8 +53,24 @@ describe "User pages" do
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_success_message('Welcome') }
-      end
-      
+      end    
+    end
+  end
+  
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
     end
   end
   
